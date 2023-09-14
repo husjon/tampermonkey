b0786b0fca833d435dbe8dbc5ac0319ee9bd4637
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2023-09-15 00:44:44 +0200

    added userscript template

---
+++ dev/userscript.template.ts
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name            TEMPLATE
+// @namespace       https://github.com/husjon/tampermonkey
+// @version         0.1.0
+// @description     DESCRIPTION
+// @author          @husjon
+// @updateURL       https://github.com/husjon/tampermonkey/raw/main/TEMPLATE.js
+// @downloadURL     https://github.com/husjon/tampermonkey/raw/main/TEMPLATE.js
+// @supportURL      https://github.com/husjon/tampermonkey/issues/new?title=TEMPLATE_SCRIPT%20v0.1.0%20-%20
+// @match           URL_TO_MATCH
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=DOMAIN_TO_MATCH
+// @grant           none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+})();
